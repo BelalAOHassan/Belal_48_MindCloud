b9a5904401c97c35cf7ec51a991f3a6bc809e904
BelalAOHassan <gute.3d.anke@gmail.com>
2022-10-21 22:24:30 +0200

    Revert "adding personal info"
    This reverts commit 9e76f2f0cbe26a488ead90a1f357c0f335076585.

--- belal.py
+++ belal.py
@@ -1,14 +1,9 @@
 
+dictionary = {"name":"Belal","Age":"20"}
+print(dictionary)
 
-personal_Data = {
-"Name":"Belal",
-"Age":20 ,
-"dept":"POWER"
-,
-"graduation year":
-2025}
-
-print(personal_Data)
+college = "Alex Uni for Engineering"
+print(college)
 
 
 
